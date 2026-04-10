a6c9f172198769967ca0461552e632346e978fa9
Ankit-976 <ankitbasa113@gmail.com>
2026-04-11 00:51:55 +0530

    Fixed login bug in frontend

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import AppRoutes from './routes/AppRoutes'
 import './styles/theme.css'
+import axios from 'axios'
 
 const App = () => {
+
+  axios.defaults.withCredentials = true;
   return (
     <div>
       <AppRoutes />
